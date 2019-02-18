3ad2f26563467a56379441d149c90ee027f3e99d
Ziya Orhan <Ziya Orhan@DESKTOP-7M88C4H>
2019-02-19 01:38:36 +0300

    project completed

---
+++ Problem1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+//Write three functions that compute the sum of the numbers in a given list using a for-loop, and recursion.
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberList = { 1, 3, 7, 40, 300 };
+            //1
+            Console.WriteLine("Using the recursive method: {0}", SumWithRecursion(numberList).ToString());
+            //2
+            Console.WriteLine("Using the for-loop method: {0}", SumWithForLoop(numberList).ToString());
+            //3
+            Console.WriteLine("Using the foreach-loop method: {0}", SumWithForeachLoop(numberList).ToString());
+            //4
+            Console.WriteLine("Using the while-loop method: {0}", SumWithWhileLoop(numberList).ToString());
+            Console.ReadLine();
+        }
+        //1- recursion method
+        static int SumWithRecursion(int[] numberList, int itemIndex = 0)
+        {
+            if (itemIndex >= numberList.Length)
+            {
+                return 0;
+            }
+            return numberList[itemIndex] + SumWithRecursion(numberList, itemIndex + 1);
+        }
+
+        //2- for-loop method
+        static int SumWithForLoop(int[] numberList)
+        {
+            int sum = 0;
+            for (int i = 0; i < numberList.Length; i++)
+            {
+                sum += numberList[i];
+            }
+            return sum;
+        }
+
+        //3- foreach-loop method
+        static int SumWithForeachLoop(int[] numberList)
+        {
+            int sum = 0;
+            foreach (int number in numberList)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+        //4- while-loop method
+        static int SumWithWhileLoop(int[] numberList)
+        {
+            int sum = 0, itemIndex = 0;
+            while (itemIndex < numberList.Length)
+            {
+                sum += numberList[itemIndex];
+                itemIndex++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+//Write a function that combines two lists by alternatingly taking elements from each of them.For example: given the two lists[a, b, c] and[1, 2, 3], the function should return [a, 1, b, 2, c, 3].
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list1;
+            int[] list2;
+            string[] ResultList;
+
+            //1- list1.Length==list2.Length
+            list1 = new string[] { "a", "b", "c" };
+            list2 = new int[] { 1, 2, 3 };
+            ResultList = AlternatinglyMerge(list1, list2);
+            ListWriter(ResultList, "1- list1.Length== list2.Length :");
+
+            //2- list1.Length > list2.Length
+            list1 = new string[] { "a", "b", "c", "d", "e", "f" };
+            list2 = new int[] { 1, 2, 3 };
+            ResultList = AlternatinglyMerge(list1, list2);
+            ListWriter(ResultList, "2- list1.Length > list2.Length :");
+
+            //3- list1.Length < list2.Length
+            list1 = new string[] { "a", "b", "c" };
+            list2 = new int[] { 1, 2, 3, 4, 5, 6 };
+            ResultList = AlternatinglyMerge(list1, list2);
+            ListWriter(ResultList, "3- list1.Length < list2.Length :");
+
+            Console.ReadLine();
+        }
+        static string[] AlternatinglyMerge(string[] list1, int[] list2)
+        {
+            string[] list3 = new string[list1.Length + list2.Length];
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            //if list1.Length is equal to list2.Length, it is entered.
+            while (i < list1.Length && j < list2.Length)
+            {
+                list3[k++] = list1[i++];
+                list3[k++] = list2[j++].ToString();
+            }
+            //if list1.Length is greater than list2.Length, it is entered.
+            while (i < list1.Length)
+            {
+                list3[k++] = list1[i++];
+            }
+            //if list1.Length is smaller than list2.Length, it is entered.
+            while (j < list2.Length)
+            {
+                list3[k++] = list2[j++].ToString();
+            }
+            return list3;
+        }
+        static void ListWriter(string[] list, string text)
+        {
+            Console.Write(text);
+            foreach (string item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
